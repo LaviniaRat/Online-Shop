4dd7a13ed7058c8c7b377fb18317ba5f6af47868
Lavinia Rat <ioana.lavinia.rat@gmail.com>
2022-08-03 19:04:02 +0300

    Add gitignore

--- src/main/java/com/onlineShop/OnlineShopController.java
+++ src/main/java/com/onlineShop/OnlineShopController.java
@@ -1,4 +1,15 @@
 package com.onlineShop;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+@Controller
 public class OnlineShopController {
+    @GetMapping("/home")
+    public String homePage(Model model){
+        model.addAttribute("product");
+        return "home.html";
+    }
 }
---
+++ src/main/java/com/onlineShop/Product.java
@@ -0,0 +1,5 @@
+package com.onlineShop;
+
+public class Product {
+
+}
